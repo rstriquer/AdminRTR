4b88b8ea1df3cae95a79b4eca3a547fadf832421
rstriquer <rstriquer@gmail.com>
2024-11-23 22:31:13 -0300

    feat: #1 Update App component to incorporate Header, SidebarNav, Home  and Footer components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './components/Header';
+import MainSidebar from './components/MainSidebar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <MainSidebar />
+      <Home />
+      <Footer />
     </div>
   );
 }
